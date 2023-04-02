38dcf8daf46f924708148346e316ec6778ab20d2
Aleksandr Savchenko <titamoto@gmail.com>
2023-04-02 10:35:54 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(array) {
+    const result = array.find(({result}) => result === "W");
+    if (array.find(({result}) => result === "W")) {
+        return result.year;
+    } else { return undefined }
+    
+}
